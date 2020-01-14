4fc9c2e765968c3268bd02e78f5243b7ad7c4584
Nicolas Sanabria Ortiz <nicolas.sanabria@correo.icesi.edu.co>
2020-01-14 09:28:02 -0800

    Uploaded full app

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+  
+  heroes:any[] = [];
+
+  constructor( private _heroesService:HeroesService,
+                private router:Router) { }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  openHeroe(index:number){
+    this.router.navigate(['/heroe',index])
+  }
+}
